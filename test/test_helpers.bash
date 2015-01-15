d368bac5292a8dc3d6ff3b0248f34fe7898d6657
Larry Gilbert <larry@industrialtoys.com>
2015-01-15 10:15:30 -0800

    Update and add to comments in test_helpers.bash

--- test/test_helpers.bash
+++ test/test_helpers.bash
@@ -1,7 +1,11 @@
-# Switch between testing gitstatus.sh and gitstatus.py
+# GIT_PROMPT_TEST_GITSTATUS_EXT can be set to override the default
+# of "sh"
 if [ -z $GIT_PROMPT_TEST_GITSTATUS_EXT ]; then
   GIT_PROMPT_TEST_GITSTATUS_EXT=sh
 fi
 
+# The filename of the script, useful for including in test messages
 gitstatus_basename="gitstatus.$GIT_PROMPT_TEST_GITSTATUS_EXT"
+
+# The full path to the script
 gitstatus="$BATS_TEST_DIRNAME/../$gitstatus_basename"
